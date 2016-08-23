cf73b797beafa91ccf4c6ed27b1af6c8697e6206
Dianne Laguerta <diannelaguerta@gmail.com>
2016-08-23 13:34:06 -0700

    wip

--- word_guess.rb
+++ word_guess.rb
@@ -1,17 +1,23 @@
+require 'csv'
+#require_relative 'words.csv'
 class WordGuess
   def initialize(debug = false)
     # are we in debug mode?
     @debug = debug
 
     # possible words, selected at random
-    @words = {
-      "e" => %w(dog cat bug hat cap lit kin fan fin fun tan ten tin ton),
-      "m" => %w(plain claim brine crime alive bride skine drive slime stein jumpy),
-      "h" => %w(
-          machiavellian prestidigitation plenipotentiary quattuordecillion
-          magnanimous unencumbered bioluminescent circumlocution
-        )
-    }
+    @words = []
+      CSV.open("words.csv", 'r').each do |line, words|
+          puts "mode is #{line[0]}"
+          @words << words
+      end
+      # "e" => %w(dog cat bug hat cap lit kin fan fin fun tan ten tin ton),
+      # "m" => %w(plain claim brine crime alive bride skine drive slime stein jumpy),
+      # "h" => %w(
+      #     machiavellian prestidigitation plenipotentiary quattuordecillion
+      #     magnanimous unencumbered bioluminescent circumlocution
+      #   )
+
 
     # players attempts allowed by difficulty
     @tries = {
